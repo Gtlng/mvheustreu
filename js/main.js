77e83086c07e98ff1a4e2242e823ced25e3a7f61
unknown <johannes.guetling@outlook.com>
2020-02-27 16:00:37 +0100

    main.js update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-fetch('https://gtlng.github.io/mvheustreu/js/htmlcomponents/footer.html').then(function (response) {
+// Holt den Header
+fetch('https://gtlng.github.io/mvheustreu/js/htmlcomponents/header.html').then(function (response) {
+    return response.text();
+}).then(function (html) {
+    document.getElementById("header").innerHTML = html;
+    // Funktion, um das Menüband in der mobilen Ansicht ausklappen zu können
+    (function (window, document) {
+        document.getElementById('toggle').addEventListener('click', function (e) {
+            document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+            document.getElementById('toggle').classList.toggle('x');
+        });
+    })(this, this.document);
+    // Funktion, um die aktuell aufgerufene Seite im Menüband hervorzuheben; setzt eine id im head und in der Liste der Navigationselemente voraus
+    var headID = document.head.id;
+    document.getElementById(headID + "_nav").className += " pure-menu-selected";
 
+}).catch(function (err) {
+    // There was an error
+    console.warn('Something went wrong.', err);
+});
+
+
+// Holte den Footer
+fetch('https://gtlng.github.io/mvheustreu/js/htmlcomponents/footer.html').then(function (response) {
     // The API call was successful!
     return response.text();
 }).then(function (html) {
@@ -8,33 +30,6 @@ fetch('https://gtlng.github.io/mvheustreu/js/htmlcomponents/footer.html').then(f
     // console.log(html);
     document.getElementById("mv-footer").innerHTML = html;
     //newHTML = html;
-}).catch(function (err) {
-    // There was an error
-    console.warn('Something went wrong.', err);
-});
-
-
-fetch('https://gtlng.github.io/mvheustreu/js/htmlcomponents/header.html').then(function (response) {
-
-    // The API call was successful!
-    return response.text();
-}).then(function (html) {
-
-    // This is the HTML from our response as a text string
-
-    document.getElementById("header").innerHTML = html;
-    //newHTML = html;
-    (function (window, document) {
-        document.getElementById('toggle').addEventListener('click', function (e) {
-            document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
-            document.getElementById('toggle').classList.toggle('x');
-        });
-    })(this, this.document);
-
-    var headID = document.head.id;
-    console.log(headID);
-    document.getElementById(headID+"_nav").className += " pure-menu-selected";
-    
 }).catch(function (err) {
     // There was an error
     console.warn('Something went wrong.', err);
